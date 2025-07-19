b1ffe5abb4e1fd0e8b4054642b2197b982ebd88f
Kadxy <2230318258@qq.com>
2025-07-19 19:02:05 +0800

    feat(prisma): add support for linux-musl-openssl-3.0.x binary target in client and schema configurations

--- prisma/main/generated/client.ts
+++ prisma/main/generated/client.ts
@@ -39,6 +39,10 @@ export { Prisma }
 path.join(__dirname, "libquery_engine-darwin-arm64.dylib.node")
 path.join(process.cwd(), "prisma/main/generated/libquery_engine-darwin-arm64.dylib.node")
 
+// file annotations for bundling tools to include these files
+path.join(__dirname, "libquery_engine-linux-musl-openssl-3.0.x.so.node")
+path.join(process.cwd(), "prisma/main/generated/libquery_engine-linux-musl-openssl-3.0.x.so.node")
+
 /**
  * Model User
  * 
